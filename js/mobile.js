4fd2d2e027c0de1f2e9a569014e83bc56cd022f2
ekfitz <51545728+cultofsnake@users.noreply.github.com>
2019-11-20 09:50:53 -0700

    Begin of mobile only
    White logo and looking pretty good

---
+++ js/mobile.js
@@ -0,0 +1,20 @@
+var menu = document.getElementById("menu");
+var logo1 = document.getElementById("logo1");
+var nav1 = document.getElementById("nav1");
+var nav2 = document.getElementById("nav2");
+var nav3 = document.getElementById("nav3");
+
+nav1.style.top = "-20%";
+nav2.style.top = "-20%";
+nav3.style.top = "-20%";
+
+
+logo1.innerHTML = "t b g";
+nav1.innerHTML = "Press";
+nav2.innerHTML = "Studio";
+nav3.innerHTML = "About";
+
+
+
+// new logo position will be top: 0.5%, left: 3%,
+// font size: 2em;
