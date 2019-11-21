b83fc2d46bfb596ee356e943f596a86b66a8fb08
Eric <57112072+ekfitz@users.noreply.github.com>
2019-11-20 21:34:56 -0700

    Second page
    Changed second page to UX design. Added new circles

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Nov 20 mobile tbg
  Mobile layout
+
+Added a bunch of stuff to the second page. Second page will be UX. Third UI. Fourth something more. 
--- js/mobile.js
+++ js/mobile.js
@@ -6,9 +6,10 @@ var nav3 = document.getElementById("nav3");
 var t1 = document.getElementById("t1");
 var t2 = document.getElementById("t2");
 var b1 = document.getElementById("b1");
+var t3 = document.getElementById("t3");
 
-logo1.style.top = "2%";
-logo1.style.fontSize = "5em";
+logo1.style.fontSize = "2em";
+logo1.style.top = "0.5%";
 nav1.style.top = "-5%";
 nav2.style.top = "-5%";
 nav3.style.top = "-5%";
@@ -21,26 +22,21 @@ nav3.innerHTML = "About";
 t1.innerHTML = "Something Motivational Right Here";
 t2.innerHTML = "Maybe some smaller text here to describe what happens when you push the button. It can be in a different font";
 b1.innerHTML = "Start Today";
+t3.innerHTML = "ux design";
+
 menu.onclick = showMenu;
 
 function showMenu() {
   if (nav1.style.top == "-5%") {
-    nav1.style.top = "2%";
-    nav2.style.top = "2%";
-    nav3.style.top = "2%";
-    logo1.style.fontSize = "2em";
-    logo1.style.top = "0.5%";
+    nav1.style.top = "2.5%";
+    nav2.style.top = "2.5%";
+    nav3.style.top = "2.5%";
     menu.style.transform = "rotate(-90deg)";
   } else {
     nav1.style.top = "-5%";
     nav2.style.top = "-5%";
     nav3.style.top = "-5%";
-    logo1.style.fontSize = "5em";
-    logo1.style.top = "2%";
     menu.style.transform = "rotate(0deg)";
 
   }
 }
-
-// new logo position will be top: 0.5%, left: 3%,
-// font size: 2em;
